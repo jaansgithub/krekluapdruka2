7ecc10853e18348cb31e029601fcd511f2664868
Jānis <98029588+jaansgithub@users.noreply.github.com>
2022-01-19 14:13:17 +0000

    progress 5

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-#grupa 2 | Jānis Dejus|Daniels Stūre|Nils Bebrītis|
-def pasuti_tkreklus(krekls, cena)
-summa = 0
-for krekla krāsa:
-  daudzums = krāsa[izmērs]
-  summa += daudzums * cena[izmērs]
-  print(summa)
-return summa
-def izmaksas(krekla_materiāls)
-izmaksas_daudzums = izmaksas_krāsa, 
+def izmaksas_kreklam(krekls, cena):
+    summa = 0
+    for izmērs in krekls:
+        daudzums = krekls[apdruka]
+        summa += daudzums * cena[apdruka]
+        print(summa)
+    return summa
+def izmaksas_kopa(izmērs, krekls, cenas):
+    izmaksas_daudzums = izmaksas_kreklam(krekls, cenas) / krekls["TEKSTS"]
+    print("Funkcijas rezultats" + str(izmaksas_kreklam(krekls, cenas)))
+    print("M izmērs" + str(krekls["TEKSTS"]))
+    krekla_izmaksas = kreklu_daudzums_ * izmaksas_daudzums
+    print("Uz {} kreklu izmaksas būs {:.2f} EUR".format(izmērs, krekla_izmaksas))
+cenas1 = {"TEKSTS": 5, "foto": 20, "zime": 7 }
+cenas2 = {"TEKSTS": 5, "foto": 20, "zime": 7 }
+if summa<50
+print("Piegāde ir 15 EUR")
+if summa>50
+print("Piegāde ir par brīvu")
+
+#izmaksas_kopa(15.0, recepte1, cenas1)
+izmaksas_kopa(15.0, cenas1, cenas2)
